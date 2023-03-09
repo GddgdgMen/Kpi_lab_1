2df8e27db0bb518ddee64ec3270ce2f7ac6dc876
Danylo Holets <goletsdan@gmail.com>
2023-03-09 19:40:26 +0200

    added boot.go

---
+++ boot.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func Start(port int) {
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		errorHandler(err)
+	}
+}
